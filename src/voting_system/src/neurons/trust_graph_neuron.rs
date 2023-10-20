b175ed57520c36fccc907f1718cddc19993e7a9f
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-10-20 15:17:59 +0200

    fix bug with empty trust map

--- src/voting_system/src/neurons/trust_graph_neuron.rs
+++ src/voting_system/src/neurons/trust_graph_neuron.rs
@@ -8,7 +8,11 @@ pub fn oracle_function(env: Env, voter_id: String) -> Result<(u32, u32), VotingS
   let external_data_provider_client =
     external_data_provider_contract::Client::new(&env, &external_data_provider_address);
 
-  let rank = Rank::from_pages(&env, external_data_provider_client.get_trust_map());
+  let trust_map = external_data_provider_client.get_trust_map();
+  if trust_map.is_empty() {
+    return Ok((0, 0));
+  }
+  let rank = Rank::from_pages(&env, trust_map);
   // TODO: consider caching the result in external data provider so the algorithm's not run every time this neuron is executed
   let rank = rank.calculate(&env).get(voter_id.clone()).unwrap_or((0, 0));
 
